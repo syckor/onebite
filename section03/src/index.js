72b2400b73d5a3c34043f25a00ad6f70bad9df41
YOUNG CHEOL SEO <syckor@naver.com>
2024-12-29 21:41:33 +0900

    studying cms system

---
+++ section03/src/index.js
@@ -0,0 +1,3 @@
+const {add, sub} = require("./math");
+console.log(add(1,2));
+console.log(sub(2,1));
---
+++ section03/src/math.js
@@ -0,0 +1,14 @@
+//math module
+
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+module.exports = {
+    add,
+    sub,
+};
